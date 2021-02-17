3ab6b9417399462e20697db3b099760504b2e9a7
Przemek Nosek <przemo99.10@gmail.com>
2021-02-18 00:11:46 +0100

    Dodanie solucji z internetu z bitwise XOR oraz mojej wlasnej ktora wymyslilem (solution2).

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
         var solution = new Solution();
-        int[] abc = {9,3,9,3,9,7,9,9999999};
+        int[] abc = {9,3,9,3,9,7,9};
+        System.out.println(solution.solution2(abc));
     }
 
 
@@ -22,5 +25,28 @@ public class Solution {
             return integer;
         }
         return 0;
+
+/*        int element = 0;
+        for (int i = 0; i < A.length; i++) {
+            element ^= A[i];
+        }
+        return element;*/
+    }
+
+    public int solution2(int[] A) {
+        Arrays.sort(A);
+        for (int i = 0; i < A.length-2; i+=2) {
+            if (A[i] != A[i+1])
+                return A[i];
+        }
+        return A[A.length-1];
+    }
+
+    public int solution3(int[] A) {
+        int element = 0;
+        for (int j : A) {
+            element ^= j;
+        }
+        return element;
     }
 }
